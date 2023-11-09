a83d03701d23d485c86ab162ba488aa740bb7657
teodoraspasojevic <spasojevic.teodora@gmail.com>
2023-11-09 20:48:28 +0100

    Added class Image Generator
    Class Image Generator added with its function next and current_epoch.

--- exercise0/generator.py
+++ exercise0/generator.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 # In this exercise task you will implement an image generator. Generator objects in python are defined as having a next function.
 # This next function returns the next generated object. In our case it returns the input of a neural network each time it gets called.
 # This input consists of a batch of images and its corresponding labels.
+
+
 class ImageGenerator:
     def __init__(self, file_path, label_path, batch_size, image_size, rotation=False, mirroring=False, shuffle=False):
         # Define all members of your generator class object as global members here.
@@ -20,16 +22,58 @@ class ImageGenerator:
 
         self.class_dict = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 5: 'dog', 6: 'frog',
                            7: 'horse', 8: 'ship', 9: 'truck'}
-        #TODO: implement constructor
+        self.file_path = file_path
+        self.label_path = label_path
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.rotation = rotation
+        self.mirroring = mirroring
+        self.shuffle = shuffle
+        self.labels = self.load_labels()
+        self.image_paths = self.load_image_paths()
+        self.epoch = 0
+        self.index = 0
+
+    def load_labels(self):
+        with open(self.file_path, 'r') as json_file:
+            labels = json.load(json_file)
+        return labels
+
+    def load_image_paths(self):
+        data = []
+        for filename, label in self.labels.items():
+            image_path = os.path.join(self.file_path, f"{filename}.npy")
+            data.append(image_path)
+        return np.array(data)
+
+    def load_images(self, start, end):
+        # YOU ARE DOING RESIZING HERE!!!
+        images = [scipy.misc.imresize(np.load(image_path), self.image_size[0:2]) for image_path in self.image_paths[start:end]]
+        return np.array(images)
 
     def next(self):
         # This function creates a batch of images and corresponding labels and returns them.
         # In this context a "batch" of images just means a bunch, say 10 images that are forwarded at once.
         # Note that your amount of total data might not be divisible without remainder with the batch_size.
         # Think about how to handle such cases
-        #TODO: implement next method
-        pass
-        #return images, labels
+
+        start = self.index
+        end = min(self.index + self.batch_size, len(self.labels))
+
+        images = self.load_images(start, end)
+        labels = np.array([self.labels[str(index)] for index in range(start, end)])
+
+        if (self.index + self.batch_size) > len(self.labels):
+            # TODO: call shuffle
+            self.epoch += 1
+            start = 0
+            end = self.index + self.batch_size - len(self.labels)
+            images = np.append(images, self.load_images(start, end), axis=0)
+            labels = np.append(labels, np.array([self.labels[str(index)] for index in range(start, end)]), axis=0)
+
+        self.index = (self.index + self.batch_size) % len(self.labels)
+
+        return images, labels
 
     def augment(self,img):
         # this function takes a single image as an input and performs a random transformation
@@ -39,8 +83,7 @@ class ImageGenerator:
         return img
 
     def current_epoch(self):
-        # return the current epoch number
-        return 0
+        return self.epoch
 
     def class_name(self, x):
         # This function returns the class name for a specific input
