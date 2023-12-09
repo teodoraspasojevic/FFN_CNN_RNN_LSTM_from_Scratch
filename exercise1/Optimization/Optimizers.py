ab69e1c9e5c2d938f2aa021dc7e86ee257ab0cf9
teodoraspasojevic <spasojevic.teodora@gmail.com>
2023-12-09 13:41:26 +0100

    Added Adam and SGD with Momentum optimizers

--- exercise1/Optimization/Optimizers.py
+++ exercise1/Optimization/Optimizers.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Sgd:
     """
     Optimizer that updates the weight tensor using stochastic gradient descent.
@@ -21,4 +24,31 @@ class Sgd:
         """
         updated_weight_tensor = weight_tensor - self.learning_rate * gradient_tensor
 
+        return updated_weight_tensor
+
+
+class SgdWithMomentum:
+    def __init__(self, learning_rate, momentum_rate):
+        self.learning_rate = learning_rate
+        self.momentum_rate = momentum_rate
+        self.momentum = 0
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        self.momentum = self.momentum_rate * self.momentum - self.learning_rate * gradient_tensor
+        updated_weight_tensor = weight_tensor + self.momentum
+        return updated_weight_tensor
+
+
+class Adam:
+    def __init__(self, learning_rate, mu, rho):
+        self.learning_rate = learning_rate
+        self.mu = mu
+        self.rho = rho
+        self.momentum = 0
+        self.second_momentum = 0
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        self.momentum = self.mu * self.momentum + (1 - self.mu) * gradient_tensor
+        self.second_momentum = self.rho * self.second_momentum + (1 - self.rho) * gradient_tensor * gradient_tensor
+        updated_weight_tensor = weight_tensor - self.learning_rate * self.momentum / (np.sqrt(self.second_momentum) + np.finfo.eps)
         return updated_weight_tensor
