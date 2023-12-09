c5cd1b0fbc4144a2207c55727c8b9ec41740a22e
teodoraspasojevic <spasojevic.teodora@gmail.com>
2023-12-09 12:49:26 +0100

    Added Initializators classes

---
+++ exercise1/Layers/Initializers.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+class Constant:
+    def __init__(self, initialization_value=0.1):
+        self.initialization_value = initialization_value
+
+    def initialize(self, weights_shape, fan_in, fan_out):
+        weights = np.ones_like(weights_shape) * self.initialization_value
+        return weights
+
+
+class UniformRandom:
+    def __init__(self):
+        pass
+
+    def initialize(self, weights_shape, fan_in, fan_out):
+        weights = np.random.uniform(0, 1, weights_shape)
+        return weights
+
+
+class Xavier:
+    def __init__(self):
+        pass
+
+    def initialize(self, weights_shape, fan_in, fan_out):
+        mean = 0
+        std = np.sqrt(2 / (fan_out + fan_in))
+        weights = np.random.normal(mean, std, weights_shape)
+        return weights
+
+
+class He:
+    def __init__(self):
+        pass
+
+    def initialize(self, weights_shape, fan_in, fan_out):
+        mean = 0
+        std = np.sqrt(2 / fan_in)
+        weights = np.random.normal(mean, std, weights_shape)
+        return weights
