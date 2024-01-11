175c7f86b8adbbe045ad074524b63b170efec8ee
teodoraspasojevic <spasojevic.teodora@gmail.com>
2024-01-11 17:41:24 +0100

    Added files for implementing RNN and Regularization

--- exercise1/Layers/__init__.py
+++ exercise1/Layers/__init__.py
@@ -1,1 +1,3 @@
-__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Conv", "Pooling", "Initializers", "Flatten", "Base"]
+# __all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Flatten", "TanH", "Sigmoid", "RNN",
+#            "Conv", "Pooling", "Initializers", "Dropout", "BatchNormalization", "Base", "LSTM"]
+__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Flatten", "Conv", "Pooling", "Initializers", "Base"]
